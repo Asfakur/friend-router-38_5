babd746973a5ec35c77f1c532a7e30247563f11e
Asfakur <saikat.hap@gmail.com>
2021-03-09 21:24:39 +0600

    38_5-2 setup simple react router with root and no match

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
-import Friend from './Friend/Friend';
+import Home from './components/Home/Home';
+import NoMatch from './components/NoMatch/NoMatch';
 
 
 function App() {
-  const [friends, setFriends] = useState([]);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => setFriends(data))
-  }, [])
 
   return (
-    <div >
-      <h1>Friends : {friends.length}</h1>
-      {
-        friends.map(friend =><Friend friend={friend} key={friend.id}></Friend>)
-      }
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>        
+        </Route>
+
+        <Route exact path="/">
+          <Home></Home>        
+        </Route>
+
+        <Route path="*">
+          <NoMatch/>       
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
